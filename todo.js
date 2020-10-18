9fea74cdf475d69926562349790d0d152d5838ff
susu-cake <72480554+susu-cake@users.noreply.github.com>
2020-10-19 00:34:43 +0900

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,71 @@
+const toDoForm = document.querySelector(".js-toDoForm"),
+    toDoInput = toDoForm.querySelector("input"),
+    toDoList = document.querySelector(".js-toDoList");
+
+
+
+function loadToDos() {
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    if (loadedToDos !== null) {
+        const parsedToDos = JSON.parse(loadedToDos);
+        parsedToDos.forEach(function (toDo) {
+            paintToDo(toDo.text);
+        });
+    }
+}
+
+const TODOS_LS = "toDos";
+
+let toDos = [];
+
+function deleteToDo(event) {
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+    const cleanToDos = toDos.filter(function(todo){
+        console.log(todo.id, li.id);
+        return todo.id !== parseInt(li.id);
+    });
+    toDos = cleanToDos
+    saveToDos();
+}
+
+
+function saveToDos() {
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
+function paintToDo(text) {
+    const toDoLi = document.createElement("li");
+    const toDoDelBtn = document.createElement("button");
+    toDoDelBtn.innerText = "❌";
+    toDoDelBtn.addEventListener("click", deleteToDo);
+    const toDoSpan = document.createElement("span");
+    const newId = toDos.length + 1;
+    toDoSpan.innerText = text;
+    toDoLi.appendChild(toDoSpan);
+    toDoLi.appendChild(toDoDelBtn);
+    toDoLi.id = newId;
+    toDoList.appendChild(toDoLi);
+    const toDoObj = {
+        text: text,
+        id: newId
+    };
+    toDos.push(toDoObj);
+    saveToDos();
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const currentValue = toDoInput.value;
+    paintToDo(currentValue);
+    toDoInput.value = "";
+}
+
+
+function init() {
+    loadToDos();
+    toDoForm.addEventListener("submit", handleSubmit)
+}
+
+init();
---
+++ weather.js
@@ -0,0 +1,56 @@
+const weather = document.querySelector(".js-weather")
+
+const COORDS = 'coords';
+const API_KEY = '4061d9f9040aa8e0dd1a8461753b292e'
+
+function getWeather(lat, lng) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    ).then(function(reponse) {
+        return reponse.json()
+    }).then(function(json){
+        const temperature = json.main.temo;
+        const place = json.name;
+        weather.innterText = `${temperature}@${place}`;
+    });
+}
+
+function saveCoords(coordsObj) {
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSuccess(position){
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude: latitude,
+        longitude: longitude
+    };
+    saveCoords(coordsObj);
+    getWeather(latitude, longitude)
+}
+
+function handleGeoError(position){
+    console.log('Cant access geo location');
+}
+
+function askForCoords() {
+    navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+}
+
+function loadCoords() {
+    const loadedCoords = localStorage.getItem(COORDS);
+    if(loadedCoords === null){
+        askForCoords();
+    } else {
+        const parsedcoords = JSON.parse(loadedCoords);
+        getWeather(parsedcoords.latitude, parsedcoords.longitude);
+    }
+}
+
+
+
+function init() {
+    loadCoords();
+}
+
+init();
